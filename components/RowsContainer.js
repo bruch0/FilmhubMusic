f007052dc8056d1f525071cefd759a47f307f22d
bruch0 <lucas.bruch0@gmail.com>
2022-02-11 20:58:39 -0300

    feat: add api request, filter by genre and map to genreRow

--- components/RowsContainer.js
+++ components/RowsContainer.js
@@ -1,12 +1,33 @@
+import { useEffect, useState } from "react"
 import styled from "styled-components"
 
 import GenreRow from "./GenreRow"
+import { getTopSongs } from "../service/api"
 
 export default function RowsContainer() {
+  const [musics, setMusics] = useState([]);
+  const [musicPerGenre, setMusicPerGenre] = useState([]);
+
+  useEffect((() => {
+    setMusics(getTopSongs().feed.results);
+
+    const hashMap = {};
+
+    musics.map((music) => {
+      const genres = music.genres;
+
+      genres.forEach((genre) => {
+        hashMap[genre.genreId] = hashMap[genre.genreId] ? {title: genre.name, musics: [...hashMap[genre.genreId].musics, music]} : {title: genre.name, musics: [music]}
+      })
+    })
+
+    setMusicPerGenre(hashMap);
+  }), [])
+
+
   return (
     <Container>
-        <GenreRow genre={'Genre name'} />
-        <GenreRow genre={'Genre name'} />
+      {Object.keys(musicPerGenre).map((key) => <GenreRow genre={musicPerGenre[key].title} musics={musicPerGenre[key].musics} />)}
     </Container>
   )
 }
