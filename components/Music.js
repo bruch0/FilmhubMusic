1e06377675d5bff6e367ff32414cbe8fa9e4cbb5
bruch0 <lucas.bruch0@gmail.com>
2022-02-11 20:46:49 -0300

    feat: add props to music component

--- components/Music.js
+++ components/Music.js
@@ -1,11 +1,11 @@
 import styled from "styled-components"
 
-export default function Music() {
+export default function Music({name, artist, url, image}) {
   return (
-      <Wrapper>
-          <AlbumImage src="https://is5-ssl.mzstatic.com/image/thumb/Music116/v4/e6/52/54/e6525474-f011-257e-04b7-12ecbae264e1/810043689519.jpg/100x100bb.jpg" />
-          <AlbumName>Album name</AlbumName>
-          <ArtistName>Artist name</ArtistName>
+      <Wrapper href={url} target='_blank'>
+          <AlbumImage src={image} />
+          <AlbumName>{name}</AlbumName>
+          <ArtistName>{artist}</ArtistName>
       </Wrapper>
   )
 }
@@ -13,7 +13,6 @@ export default function Music() {
 const Wrapper = styled.a`
     width: 13%;
     min-width: 13%;
-    background-color: blue;
     margin: 0px 15px 0px 0px;
 
     :last-of-type {
@@ -30,9 +29,11 @@ const AlbumName = styled.h3`
     font-weight: 500;
     font-size: 22px;
     margin-bottom: 10px;
+    color: black;
 `
 
 const ArtistName = styled.h3`
     font-weight: 500;
     font-size: 18px;
+    color: black;
 `
