8f153a7cdec9e8f56060f3081b6094bebe28bdbf
bruch0 <lucas.bruch0@gmail.com>
2022-02-11 20:47:06 -0300

    feat: add musics props to genteRow

--- components/GenreRow.js
+++ components/GenreRow.js
@@ -1,16 +1,37 @@
 import styled from "styled-components"
 
-export default function GenreRow({genre}) {
-  return (
-      <>
-        <Genre>{genre}</Genre>
+import Music from "./Music"
 
-      </>
+export default function GenreRow({genre, musics}) {
+  return (
+      <Wrapper>
+        <Genre>{genre}</Genre>
+        <Musics>
+          {musics.map((music) => <Music name={music.name} artist={music.artistName} url={music.url} image={music.artworkUrl100} />)}
+        </Musics>
+      </Wrapper>
   )
 }
+
+const Wrapper = styled.section`
+  margin-bottom: 50px;
+`
 
 const Genre = styled.h2`
     width: 100%;
     font-size: 2.5vw;
     font-weight: 500;
+    margin-bottom: 20px;
+`
+
+const Musics = styled.div`
+    display: flex;
+    flex-shrink: 0;
+    overflow-x: scroll;
+    overflow-y: hidden;
+    justify-content: start;
+
+    ::-webkit-scrollbar {
+      height: 0px;
+    }
 `
