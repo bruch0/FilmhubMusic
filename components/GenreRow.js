a3f48a00e0330a387937a76661f765d6080a2836
bruch0 <lucas.bruch0@gmail.com>
2022-02-11 21:12:31 -0300

    use the filter to display the musics accordingly

--- components/GenreRow.js
+++ components/GenreRow.js
@@ -2,19 +2,23 @@ import styled from "styled-components"
 
 import Music from "./Music"
 
-export default function GenreRow({genre, musics}) {
+export default function GenreRow({genre, musics, filter}) {
   return (
-      <Wrapper>
+      <Wrapper visible={musics.filter((music) => music.name.startsWith(filter)).length}>
         <Genre>{genre}</Genre>
         <Musics>
-          {musics.map((music) => <Music name={music.name} artist={music.artistName} url={music.url} image={music.artworkUrl100} />)}
-        </Musics>
+          {filter ? 
+          musics.filter((music) => music.name.startsWith(filter))
+          .map((music) => <Music name={music.name} artist={music.artistName} url={music.url} image={music.artworkUrl100} />) 
+          :
+          musics.map((music) => <Music name={music.name} artist={music.artistName} url={music.url} image={music.artworkUrl100} />)}        </Musics>
       </Wrapper>
   )
 }
 
 const Wrapper = styled.section`
   margin-bottom: 50px;
+  display: ${props => props.visible ? '' : 'none'};
 `
 
 const Genre = styled.h2`
